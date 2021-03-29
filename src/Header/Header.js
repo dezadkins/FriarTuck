f8ac45527bd7cb6ad6ddbf504554c2abe1efdfe9
Dez <dez@dezs-mbp.attlocal.net>
2021-03-29 12:31:08 -0400

    header placed items

--- src/Header/Header.js
+++ src/Header/Header.js
@@ -1,23 +1,29 @@
 import React from "react";
 import Logo from "../assets/robinhood.svg";
 import SearchIcon from "@material-ui/icons/Search";
+import "./Header.css";
 
 function Header() {
   return (
     <div className="header__container">
-      {/* logo */}
       <div className="header__logo">
         <img src={Logo} width={25} alt="robinhood logo" />
       </div>
-      {/* search */}
+
       <div className="header__search">
         <div className="header__searchBox">
-          <SearchIcon style={{ color: "white" }} />
+          {/* <SearchIcon style={{ color: "white" }} /> */}
+          <input placeholder="search" type="text" />
         </div>
       </div>
-      {/* menuitems */}
-      <div className="header__menu"></div>
-      <h1>Header</h1>
+
+      <div className="header__menuItems">
+        <a href="#">Free Stocks</a>
+        <a href="#">Portfolio</a>
+        <a href="#">Cash</a>
+        <a href="#">Messages</a>
+        <a href="#">Account</a>
+      </div>
     </div>
   );
 }
