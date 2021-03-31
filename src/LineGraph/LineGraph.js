044165c31795e8e44a7341beb9cc4610c80b6542
Dez <dez@dezs-mbp.attlocal.net>
2021-03-31 09:57:04 -0400

    chart add

---
+++ src/LineGraph/LineGraph.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+function LineGraph() {
+  return (
+    <div>
+      <h1>Line</h1>
+    </div>
+  );
+}
+
+export default LineGraph;
--- src/NewsFeed/NewsFeed.js
+++ src/NewsFeed/NewsFeed.js
@@ -1,13 +1,18 @@
 import React from "react";
+import LineGraph from "../LineGraph/LineGraph";
 import "./NewsFeed.css";
+
 function NewsFeed() {
   return (
     <div className="newsFeed">
       <div className="newsFeed__container">
-        <div className="newsFeed__chart">
+        <div className="newsFeed__chartSection">
           <div className="chart__PortfolioValue">
-            <h1>$114,858.84</h1>
+            <h1>$114,858</h1>
             <p>+44.53 (+0.04%) Today</p>
+          </div>
+          <div className="newsFeed__chart">
+            <LineGraph />
           </div>
         </div>
       </div>
