d8b3a432f5609e1cbb8a9705c15faeee683d3955
Dez <dez@dezs-mbp.attlocal.net>
2021-03-31 12:09:17 -0400

    graph data functions

--- src/LineGraph/LineGraph.js
+++ src/LineGraph/LineGraph.js
@@ -1,25 +1,43 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { Line } from "react-chartjs-2";
 
 function LineGraph() {
-  const data = [
-    {
-      x: 13,
-      y: 20,
-    },
-    {
-      x: 15,
-      y: 10,
-    },
-    {
-      x: 12,
-      y: 4,
-    },
-    {
-      x: 16,
-      y: 7,
-    },
-  ];
+  const [graphData, setGraphData] = useState([]);
+  //   const data = [
+  //     {
+  //       x: 13,
+  //       y: 20,
+  //     },
+  //     {
+  //       x: 15,
+  //       y: 10,
+  //     },
+  //     {
+  //       x: 12,
+  //       y: 4,
+  //     },
+  //     {
+  //       x: 16,
+  //       y: 7,
+  //     },
+  //   ];
+  const mockData = () => {
+    let data = [];
+    let value = 50;
+    for (let i = 0; i < 366; i++) {
+      let date = new Date();
+      date.setHours(0, 0, 0, 0);
+      date.setDate(i);
+      value += Math.round((Math.random() < 0.5 ? 1 : 0) * Math.random() * 10);
+      data.push({ x: date, y: value });
+    }
+    setGraphData(data);
+  };
+
+  useEffect(() => {
+    mockData();
+  }, []);
+
   return (
     <div className="lineGraph">
       <Line
@@ -27,7 +45,7 @@ function LineGraph() {
           datasets: [
             {
               type: "line",
-              data: data,
+              data: graphData,
               backgroundColor: "black",
               borderColor: "#5AC53B",
               borderWidth: 2,
@@ -44,6 +62,18 @@ function LineGraph() {
           legend: { display: false },
           tooltips: { mode: "index", intersect: false },
           scales: {
+            xAxes: [
+              {
+                type: "time",
+                time: {
+                  format: "MM/DD/YY",
+                  tooltipFormat: "ll",
+                },
+                ticks: {
+                  display: false,
+                },
+              },
+            ],
             yAxes: [
               {
                 ticks: {
