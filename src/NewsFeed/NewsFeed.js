4e318abc0fdefb83b2aa406b42453b550f81759b
Dez <dez@dezs-mbp.attlocal.net>
2021-03-31 09:40:20 -0400

    chart and value sections

--- src/NewsFeed/NewsFeed.js
+++ src/NewsFeed/NewsFeed.js
@@ -3,7 +3,14 @@ import "./NewsFeed.css";
 function NewsFeed() {
   return (
     <div className="newsFeed">
-      <div className="newsFeed__container"></div>
+      <div className="newsFeed__container">
+        <div className="newsFeed__chart">
+          <div className="chart__PortfolioValue">
+            <h1>$114,858.84</h1>
+            <p>+44.53 (+0.04%) Today</p>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
--- src/Stats/Stats.js
+++ src/Stats/Stats.js
@@ -1,5 +1,6 @@
 import React from "react";
 import "./Stats.css";
+
 function Stats() {
   return (
     <div className="stats">
